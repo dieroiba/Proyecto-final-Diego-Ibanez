13e035acf71971091a15196ccdb06f3ce170b235
dieroiba <dieroiba@gmail.com>
2022-11-19 20:13:22 -0300

    Formulario de contacto

---
+++ mail.php
@@ -0,0 +1,38 @@
+<?php
+
+$name = $_POST['name']
+$email = $_POST['email']
+$subject = $_POST['subject']
+$message = $_POST['message']
+
+$mailheader = "From:".$name. "<" .$email. ">\r\n"
+
+$recipient = "dieroiba@gmail.com"; 
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+echo '
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Gracias por contactarme! Te contestaré en breve!</h1>
+        <p class="back">Volver a <a href="./index.html"></a>.</p>
+        
+    </div>
+    
+</body>
+</html>
+
+
+';
+
+
+?>
